51b8733946571d2c807c376ea9d00063ed2fb889
Gilad Ehven <gilad@ehven.com>
2018-09-18 00:48:36 -0400

    Migrated code from [pre-TIE] [TypeEight] to current/new version (TIE).

--- TIE/base/interface.php
+++ TIE/base/interface.php
@@ -14,12 +14,27 @@
 
             protected $theme_version;
 
+            protected $project_prop_theme_author       = '';
+            protected $project_prop_theme_author_uri   = '';
+            protected $project_prop_theme_contributors = '';
+            protected $project_prop_theme_description  = '';
+            protected $project_prop_theme_domain_path  = '';
+            protected $project_prop_theme_license      = '';
+            protected $project_prop_theme_license_uri  = '';
+            protected $project_prop_theme_name         = '';
+            protected $project_prop_theme_template     = '';
+            protected $project_prop_theme_text_domain  = '';
+            protected $project_prop_theme_uri          = '';
+            protected $project_prop_theme_version      = '';
+
             public function __construct() {
 
                 parent::__construct();
 
                 $this->set_text_domain();
                 $this->set_theme_version();
+
+                $this->set_theme_props();
 
             }
 
@@ -42,6 +57,28 @@
 //              $this->text_domain = $theme_object->get( 'TextDomain' );
                 $this->theme_version = $theme_object->get( 'Version' );
             }
+
+            protected function set_theme_props() {
+                $theme_object = wp_get_theme();
+                $this->project_prop_theme_author       .= $theme_object->get( 'Author' );
+                $this->project_prop_theme_author_uri   .= $theme_object->get( 'AuthorURI' );
+            //  $this->project_prop_theme_contributors .= $theme_object->get( '' );
+                $this->project_prop_theme_description  .= $theme_object->get( 'Description' );
+                $this->project_prop_theme_domain_path  .= $theme_object->get( 'DomainPath' );
+            //  $this->project_prop_theme_license      .= $theme_object->get( '' );
+            //  $this->project_prop_theme_license_uri  .= $theme_object->get( '' );
+                $this->project_prop_theme_name         .= $theme_object->get( 'Name' );
+                $this->project_prop_theme_template     .= $theme_object->get( 'Template' );
+                $this->project_prop_theme_text_domain  .= $theme_object->get( 'TextDomain' );
+                $this->project_prop_theme_uri          .= $theme_object->get( 'ThemeURI' );
+                $this->project_prop_theme_version      .= $theme_object->get( 'Version' );
+            }
+
+
+
+
+
+
 
 
         }
--- TIE/base/project.php
+++ TIE/base/project.php
@@ -10,6 +10,25 @@
 
         abstract class TIE_Project extends TIE_Base {
 
+            protected $project_prop_fqdn               = '';
+
+            protected $project_prop_server_os          = '';
+            protected $project_prop_server_type        = '';
+            protected $project_prop_server_version     = '';
+
+            protected $project_prop_social_facebook    = '';
+            protected $project_prop_social_github      = '';
+            protected $project_prop_social_instagram   = '';
+            protected $project_prop_social_linkedin    = '';
+            protected $project_prop_social_pinterest   = '';
+            protected $project_prop_social_twitter     = '';
+
+        protected $requested_resource_type;
+
+        protected $requested_resource_value;
+
+        protected $requesting_referer;
+
             public function __construct() {
 
                 parent::__construct();
