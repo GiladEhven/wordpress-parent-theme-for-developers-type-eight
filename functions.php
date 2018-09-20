a00985dd14002fa585b7834d7e051dc596e1d012
Gilad Ehven <gilad@ehven.com>
2018-09-20 00:46:07 -0400

    Cleaned up [functions.php]: Removed theme prop queries, commented old code, and other dumpables.

--- functions.php
+++ functions.php
@@ -29,22 +29,11 @@
 
     if ( ! defined( 'GILAD_WEBSITE_PHASE' ) )              define( 'GILAD_WEBSITE_PHASE',              '' );
 
-
-
     if ( ! class_exists( __NAMESPACE__ . 'Theme_Core' ) ) {
 
         class Theme_Core {
 
-            private $theme_name;
-
-            private $theme_version;
-
-            public static $object_counter = 0;
-
             public function __construct() {
-
-                $this->set_theme_name();
-                $this->set_theme_version();
 
                 if ( is_admin() ) {
 
@@ -54,42 +43,22 @@
                     require_once( get_stylesheet_directory() . '/admin/php/theme/class-theme-navigation.php' );
                     require_once( get_stylesheet_directory() . '/admin/php/theme/class-theme-support.php' );
 
-                    $admin_resources    = new Admin_Resources;
-                    $theme_admin        = new Theme_Admin;
-                    $theme_media        = new Theme_Media;
-                    $theme_navigation   = new Theme_Navigation;
-                    $theme_support      = new Theme_Support;
+                    $admin_resources  = new Admin_Resources;
+                    $theme_admin      = new Theme_Admin;
+                    $theme_media      = new Theme_Media;
+                    $theme_navigation = new Theme_Navigation;
+                    $theme_support    = new Theme_Support;
 
                 } else {
 
                     require_once( get_stylesheet_directory() . '/public/loaders/class-public-scripts.php' );
                     require_once( get_stylesheet_directory() . '/public/loaders/class-public-styles.php' );
-                    $public_scripts  = new Public_Scripts;
-                    $public_styles  = new Public_Styles;
+
+                    $public_scripts   = new Public_Scripts;
+                    $public_styles    = new Public_Styles;
+
                 }
 
-                self::$object_counter++;
-
-            }
-
-            //  -------------------------  GETTERS AND SETTERS  -------------------------  //
-
-            public function get_theme_name() {
-                return $this->theme_name;
-            }
-
-            public function get_theme_version() {
-                return $this->theme_version;
-            }
-
-            protected function set_theme_name() {
-                $theme_object = wp_get_theme();
-                $this->theme_name = $theme_object->get( 'Name' );
-            }
-
-            protected function set_theme_version() {
-                $theme_object = wp_get_theme();
-                $this->theme_version = $theme_object->get( 'Version' );
             }
 
         }
