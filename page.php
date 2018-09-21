e2d208a659a05799d09f1cf676e474a80e7ce2dd
Gilad Ehven <gilad@ehven.com>
2018-09-20 23:59:37 -0400

    Updated existing 'Page' type to match new generic (TIE) pattern.

--- page.php
+++ page.php
@@ -10,48 +10,9 @@
 
         class Template_Page extends TIE_Document {
 
-//          private $requested_resource;
-
-            //  GET META
-
-            //  GET REFERRER
-
-            //  PACKAGE DATA: CONTENT, EXCERPT, FIELDS, GET's
-
-//          public static $object_counter = 0;
-
             public function __construct() {
 
-//              $this->set_requested_resource();
-
-//          	$data = $this->package_data_for_view();
-
-//              require_once( get_stylesheet_directory() . '/public/php/views/class-view-page.php' );
-//              $view_page = new View_Page( $data );
-
-//              self::$object_counter++;
-
                 parent::__construct();
-
-            }
-
-            //  -------------------------  GETTERS AND SETTERS  -------------------------  //
-
-            public function get_requested_resource() {
-                return $this->requested_resource;
-            }
-
-            protected function set_requested_resource() {
-                $this->requested_resource = 'undetermined';
-            }
-
-            //  ----------------------------  MISSION LOGIC  ----------------------------  //
-
-            protected function package_data_for_view() {
-
-                	$data = array();
-
-                	return $data;
 
             }
 
@@ -59,26 +20,20 @@
 
     }
 
-    get_header(); ?>
 
-                <div id="main">
 
-                    <?php
+    $page = new Template_Page();
 
-                        $template_page = new Template_Page();
 
-                    ?>
 
-                </div><!-- / # main -->
+    // WITHOUT THIS CALL, THE DEFAULT ERROR MESSAGE/ARRAY REMAINS AVAILABLE:
+    $page->set_data(array(
+        'template' => 'page.php',
+        'class'    => 'Template_Page',
+        'type'     => 'Document',
+        'parent'   => 'TIE_Document',
+    ));
 
-                <?php
 
-                    if ( comments_open() || get_comments_number() ) {
 
-                        comments_template();
-
-                    }
-
-                ?>
-
-    <?php get_footer();
+    $page->get_shell( basename( __FILE__ ), 'Sitewide' );
