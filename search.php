db4722b41ef2629f157ee6efb36d32617fc434bb
Gilad Ehven <gilad@ehven.com>
2018-09-21 18:58:24 -0400

    Updated existing 'Search' type to match new generic (TIE) pattern.

--- search.php
+++ search.php
@@ -1,1 +1,39 @@
-SEARCH RESULTS
+<?php
+
+	namespace Ehven\Gilad\WordPress\Themes\Starters\TypeEight;
+
+    if ( ! defined( 'ABSPATH' ) ) exit( 'Nothing to see here. Go <a href="/">home</a>.' );
+
+    require_once( get_stylesheet_directory() . '/TIE/base/interface/public/template/default/list.php' );
+
+    if ( ! class_exists( __NAMESPACE__ . 'Template_Search' ) ) {
+
+        class Template_Search extends TIE_List {
+
+            public function __construct() {
+
+                parent::__construct();
+
+            }
+
+        }
+
+    }
+
+
+
+    $search = new Template_Search();
+
+
+
+    // WITHOUT THIS CALL, THE DEFAULT ERROR MESSAGE/ARRAY REMAINS AVAILABLE:
+    $search->set_data(array(
+        'template' => 'search.php',
+        'class'    => 'Template_Search',
+        'type'     => 'List',
+        'parent'   => 'TIE_List',
+    ));
+
+
+
+    $search->get_shell( basename( __FILE__ ), 'Sitewide' );
