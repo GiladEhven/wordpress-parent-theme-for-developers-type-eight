120379672a33379618ccec0648c7a5aab84599e1
Gilad Ehven <gilad@ehven.com>
2018-09-21 19:04:25 -0400

    Updated existing 'Taxonomy' type to match new generic (TIE) pattern.

--- taxonomy.php
+++ taxonomy.php
@@ -1,1 +1,39 @@
-Taxonomy...
+<?php
+
+	namespace Ehven\Gilad\WordPress\Themes\Starters\TypeEight;
+
+    if ( ! defined( 'ABSPATH' ) ) exit( 'Nothing to see here. Go <a href="/">home</a>.' );
+
+    require_once( get_stylesheet_directory() . '/TIE/base/interface/public/template/default/list.php' );
+
+    if ( ! class_exists( __NAMESPACE__ . 'Template_Taxonomy' ) ) {
+
+        class Template_Taxonomy extends TIE_List {
+
+            public function __construct() {
+
+                parent::__construct();
+
+            }
+
+        }
+
+    }
+
+
+
+    $taxonomy = new Template_Taxonomy();
+
+
+
+    // WITHOUT THIS CALL, THE DEFAULT ERROR MESSAGE/ARRAY REMAINS AVAILABLE:
+    $taxonomy->set_data(array(
+        'template' => 'taxonomy.php',
+        'class'    => 'Template_Taxonomy',
+        'type'     => 'List',
+        'parent'   => 'TIE_List',
+    ));
+
+
+
+    $taxonomy->get_shell( basename( __FILE__ ), 'Sitewide' );
