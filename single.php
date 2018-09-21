d12fab44367f6e37e0871071fa9ff2d141678dfe
Gilad Ehven <gilad@ehven.com>
2018-09-21 19:03:11 -0400

    Updated existing 'Single' type to match new generic (TIE) pattern.

--- single.php
+++ single.php
@@ -10,48 +10,9 @@
 
         class Template_Single extends TIE_Document {
 
-//          private $requested_resource;
-
-            //  GET META
-
-            //  GET REFERRER
-
-            //  PACKAGE DATA: CONTENT, EXCERPT, FIELDS, GET's
-
-//          public static $object_counter = 0;
-
             public function __construct() {
 
-//              $this->set_requested_resource();
-
-//          	$data = $this->package_data_for_view();
-
-//              require_once( get_stylesheet_directory() . '/public/php/views/class-view-single.php' );
-//              $view_single = new View_Single( $data );
-
-//              self::$object_counter++;
-
                 parent::__construct();
-
-            }
-
-            //  -------------------------  GETTERS AND SETTERS  -------------------------  //
-
-            public function get_requested_resource() {
-                return $this->requested_resource;
-            }
-
-            protected function set_requested_resource() {
-                $this->requested_resource = 'undetermined';
-            }
-
-            //  ----------------------------  MISSION LOGIC  ----------------------------  //
-
-            protected function package_data_for_view() {
-
-                	$data = array();
-
-                	return $data;
 
             }
 
@@ -59,26 +20,28 @@
 
     }
 
-    get_header(); ?>
+    /*
 
-                <div id="main">
+    if ( comments_open() || get_comments_number() ) {
 
-                    <?php
+        comments_template();
 
-                        $template_single = new Template_Single();
+    }
 
-                    ?>
+    */
 
-                </div><!-- / # main -->
+    $single = new Template_Single();
 
-                <?php
 
-                    if ( comments_open() || get_comments_number() ) {
 
-                        comments_template();
+    // WITHOUT THIS CALL, THE DEFAULT ERROR MESSAGE/ARRAY REMAINS AVAILABLE:
+    $single->set_data(array(
+        'template' => 'single.php',
+        'class'    => 'Template_Single',
+        'type'     => 'Document',
+        'parent'   => 'TIE_Document',
+    ));
 
-                    }
 
-                ?>
 
-    <?php get_footer();
+    $single->get_shell( basename( __FILE__ ), 'Sitewide' );
