81dd48411822e42c056d7975906b0c4f9c6ba989
Gilad Ehven <gilad@ehven.com>
2018-10-22 19:47:49 -0400

    Added 'load_project_specific_implementations' method to 'TIE_Base' class; class constructor method-call commented out pending required key changes in 'functions.php'.

--- TIE/base.php
+++ TIE/base.php
@@ -14,6 +14,48 @@
 
                 $this->object_counter++;
 
+//              $this->load_project_specific_implementations();
+
+            }
+
+            protected function load_project_specific_implementations() {
+
+                if ( is_admin() ) {
+
+                    //
+
+                    foreach( glob( GILAD_PATH_DIR_ADMIN . "/classes/*.php" ) as $file ) {
+
+                        require_once( $file );
+
+                    }
+
+                    //
+
+                } else {
+
+                    //
+
+                    foreach( glob( GILAD_PATH_DIR_PUBLIC . "/classes/*.php" ) as $file ) {
+
+                        require_once( $file );
+
+                    }
+
+                    //
+
+                }
+
+                //
+
+                foreach( glob( GILAD_PATH_DIR_COMMON . "/classes/*.php" ) as $file ) {
+
+                    require_once( $file );
+
+                }
+
+                //
+
             }
 
         }
