26159f33dae9b1a615109c28a846cb865290615c
Gilad Ehven <gilad@ehven.com>
2019-09-22 15:34:53 -0400

    Added 'archive.php' template placeholder + related 'CORE' class.

---
+++ CORE/core/website/template/list/archive.php
@@ -0,0 +1,28 @@
+<?php
+
+    namespace Ehven\Gilad\WordPress\Themes\Parents\TypeEight;
+
+    if ( ! defined( 'ABSPATH' ) ) exit( 'Nothing to see here. Go <a href="/">home</a>.' );
+
+    require_once( TYPE8_CORE_LIST . '.php' );
+
+    abstract class CORE_Archive extends CORE_List {
+
+        protected function __construct() {
+
+            parent::__construct();
+
+            $this->set_archive_properties();
+
+        }
+
+        protected function set_archive_properties() {
+
+            $this->archive_x          = '-----------------------------------------------------------------------------------------------';
+            $this->archive_y          = '--------------------------------  C O R E   :   A R C H I V E  --------------------------------';
+            $this->archive_z          = '-----------------------------------------------------------------------------------------------';
+            $this->archive_test_value = 'From CORE_Archive class...';
+
+        }
+
+    }
