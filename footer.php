5e7208df886218e988196f62360456ad51f51ede
Gilad Ehven <gilad@ehven.com>
2019-09-23 07:56:01 -0400

    Added 'header.pgp' and 'footer.php' template placeholders.

---
+++ footer.php
@@ -0,0 +1,13 @@
+<?php
+
+    namespace Ehven\Gilad\WordPress\Themes\Parents\TypeEight;
+
+    if ( ! defined( 'ABSPATH' ) ) exit( 'Nothing to see here. Go <a href="/">home</a>.' );
+
+    wp_footer(); ?>
+
+
+
+    </body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,22 @@
+<?php
+
+    namespace Ehven\Gilad\WordPress\Themes\Parents\TypeEight;
+
+    if ( ! defined( 'ABSPATH' ) ) exit( 'Nothing to see here. Go <a href="/">home</a>.' );
+
+?><!DOCTYPE html>
+<html <?php // TODO: Insert [language_attributes] ?>>
+
+    <head>
+
+        <meta charset="<?php // TODO: Insert [bloginfo|charset] ?>" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="description" content="<?php // TODO: Insert [bloginfo|description] ?>" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link rel="pingback" href="<?php // TODO: Insert [bloginfo|pingback_url] ?>" />
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+
+        <?php wp_head(); ?>
+
+    </head>
