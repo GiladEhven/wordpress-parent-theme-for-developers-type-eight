2bf9f5d582a8f93dd0c165c2eff9bfcdd9a36040
Gilad Ehven <gilad@ehven.com>
2019-09-23 07:57:01 -0400

    Added 'sidebar.php' template placeholder.

---
+++ sidebar.php
@@ -0,0 +1,11 @@
+<?php
+
+    namespace Ehven\Gilad\WordPress\Themes\Parents\TypeEight;
+
+    if ( ! defined( 'ABSPATH' ) ) exit( 'Nothing to see here. Go <a href="/">home</a>.' ); ?>
+
+
+
+<aside>
+
+</aside>
