1424df3f1a6171d3c655fd2415573efca929679d
Gilad Ehven <gilad@ehven.com>
2019-09-24 08:28:47 -0400

    Added 'text.php' template placeholder + related 'CORE' class.

---
+++ CORE/core/website/template/document/text.php
@@ -0,0 +1,28 @@
+<?php
+
+    namespace Ehven\Gilad\WordPress\Themes\Parents\TypeEight;
+
+    if ( ! defined( 'ABSPATH' ) ) exit( 'Nothing to see here. Go <a href="/">home</a>.' );
+
+    require_once( TYPE8_CORE_DOCUMENT . '.php' );
+
+    abstract class CORE_Text extends CORE_Document {
+
+        protected function __construct() {
+
+            parent::__construct();
+
+            $this->set_text_properties();
+
+        }
+
+        protected function set_text_properties() {
+
+            $this->text_x          = '-----------------------------------------------------------------------------------------------';
+            $this->text_y          = '-----------------------------------  C O R E   :   T E X T  -----------------------------------';
+            $this->text_z          = '-----------------------------------------------------------------------------------------------';
+            $this->text_test_value = 'From CORE_Text class...';
+
+        }
+
+    }
