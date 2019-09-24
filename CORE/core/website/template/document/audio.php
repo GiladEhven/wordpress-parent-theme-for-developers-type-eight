9bb7f585518cfb80e9882e06341f5e0a3ab61670
Gilad Ehven <gilad@ehven.com>
2019-09-24 08:22:32 -0400

    Added 'audio.php' template placeholder + related 'CORE' class.

---
+++ CORE/core/website/template/document/audio.php
@@ -0,0 +1,28 @@
+<?php
+
+    namespace Ehven\Gilad\WordPress\Themes\Parents\TypeEight;
+
+    if ( ! defined( 'ABSPATH' ) ) exit( 'Nothing to see here. Go <a href="/">home</a>.' );
+
+    require_once( TYPE8_CORE_DOCUMENT . '.php' );
+
+    abstract class CORE_Audio extends CORE_Document {
+
+        protected function __construct() {
+
+            parent::__construct();
+
+            $this->set_audio_properties();
+
+        }
+
+        protected function set_audio_properties() {
+
+            $this->audio_x          = '-----------------------------------------------------------------------------------------------';
+            $this->audio_y          = '----------------------------------  C O R E   :   A U D I O  ----------------------------------';
+            $this->audio_z          = '-----------------------------------------------------------------------------------------------';
+            $this->audio_test_value = 'From CORE_Audio class...';
+
+        }
+
+    }
