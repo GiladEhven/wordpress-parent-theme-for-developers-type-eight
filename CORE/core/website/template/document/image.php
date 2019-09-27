afd8e281c506a8cef5cd41cf705657e1b1ecebb5
Gilad Ehven <gilad@ehven.com>
2019-09-27 00:25:34 -0400

    Added 'image.php' template placeholder + related 'CORE' class.

---
+++ CORE/core/website/template/document/image.php
@@ -0,0 +1,28 @@
+<?php
+
+    namespace Ehven\Gilad\WordPress\Themes\Parents\TypeEight;
+
+    if ( ! defined( 'ABSPATH' ) ) exit( 'Nothing to see here. Go <a href="/">home</a>.' );
+
+    require_once( TYPE8_CORE_DOCUMENT . '.php' );
+
+    abstract class CORE_Image extends CORE_Document {
+
+        protected function __construct() {
+
+            parent::__construct();
+
+            $this->set_image_properties();
+
+        }
+
+        protected function set_image_properties() {
+
+            $this->image_x          = '-----------------------------------------------------------------------------------------------';
+            $this->image_y          = '----------------------------------  C O R E   :   A U D I O  ----------------------------------';
+            $this->image_z          = '-----------------------------------------------------------------------------------------------';
+            $this->image_test_value = 'From CORE_Image class...';
+
+        }
+
+    }
