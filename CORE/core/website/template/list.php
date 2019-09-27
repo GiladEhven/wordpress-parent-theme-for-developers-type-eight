102bdc14d86fb7b52551962bed8f259908409d8f
Gilad Ehven <gilad@ehven.com>
2019-09-27 08:01:05 -0400

    Added output fields for 'Categories' to 'CORE_List' class file.

--- CORE/core/website/template/list.php
+++ CORE/core/website/template/list.php
@@ -12,16 +12,36 @@
 
             parent::__construct();
 
-            $this->set_list_properties();
+            if ( is_category() ) $this->set_categories_properties();
 
         }
 
-        protected function set_list_properties() {
+        protected function set_categories_properties() {
+
+            $categories_array = get_categories();
+            $categories_counter = 0;
+            $categories_all = array();
+
+            foreach ( $categories_array as $category ) {
+
+                $categories_counter++;
+
+                $categories_all[$categories_counter]['description'] = $category->description;
+                $categories_all[$categories_counter]['entries']     = $category->count;
+                $categories_all[$categories_counter]['id']          = $category->term_id;
+                $categories_all[$categories_counter]['name']        = $category->name;
+                $categories_all[$categories_counter]['parent']      = $category->parent;
+                $categories_all[$categories_counter]['slug']        = $category->slug;
+                $categories_all[$categories_counter]['url']         = get_category_link( $category->term_id );
+
+            }
 
             $this->list_x          = '-----------------------------------------------------------------------------------------------';
-            $this->list_y          = '-----------------------------------  C O R E   :   L I S T  -----------------------------------';
+            $this->list_y          = '----------------------  C O R E   :   L I S T   :   C A T E G O R I E S  ----------------------';
             $this->list_z          = '-----------------------------------------------------------------------------------------------';
             $this->list_test_value = 'From CORE_List class...';
+
+            $this->categories      = $categories_all;
 
         }
 
