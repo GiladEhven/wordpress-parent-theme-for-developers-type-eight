d8a68ad377462a9949598de97e5b96b3bdf337fc
Gilad Ehven <gilad@ehven.com>
2019-09-27 00:24:31 -0400

    Added 'CORE_Front_Page' and 'CORE_Home' to 'core/website/template/front'.

---
+++ CORE/core/website/template/front/front-page.php
@@ -0,0 +1,28 @@
+<?php
+
+    namespace Ehven\Gilad\WordPress\Themes\Parents\TypeEight;
+
+    if ( ! defined( 'ABSPATH' ) ) exit( 'Nothing to see here. Go <a href="/">home</a>.' );
+
+    require_once( TYPE8_CORE_FRONT . '.php' );
+
+    abstract class CORE_Front_Page extends CORE_Front {
+
+        protected function __construct() {
+
+            parent::__construct();
+
+            $this->set_front_page_properties();
+
+        }
+
+        protected function set_front_page_properties() {
+
+            $this->front_page_x          = '-----------------------------------------------------------------------------------------------';
+            $this->front_page_y          = '-----------------------------  C O R E   :   F R O N T   P A G E  -----------------------------';
+            $this->front_page_z          = '-----------------------------------------------------------------------------------------------';
+            $this->front_page_test_value = 'From CORE_Front_Page class...';
+
+        }
+
+    }
---
+++ CORE/core/website/template/front/home.php
@@ -0,0 +1,28 @@
+<?php
+
+    namespace Ehven\Gilad\WordPress\Themes\Parents\TypeEight;
+
+    if ( ! defined( 'ABSPATH' ) ) exit( 'Nothing to see here. Go <a href="/">home</a>.' );
+
+    require_once( TYPE8_CORE_FRONT . '.php' );
+
+    abstract class CORE_Home extends CORE_Front {
+
+        protected function __construct() {
+
+            parent::__construct();
+
+            $this->set_home_properties();
+
+        }
+
+        protected function set_home_properties() {
+
+            $this->home_x          = '-----------------------------------------------------------------------------------------------';
+            $this->home_y          = '-----------------------------------  C O R E   :   H O M E  -----------------------------------';
+            $this->home_z          = '-----------------------------------------------------------------------------------------------';
+            $this->home_test_value = 'From CORE_Home class...';
+
+        }
+
+    }
