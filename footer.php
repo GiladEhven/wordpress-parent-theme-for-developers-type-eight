9b211cd5cf16efbdaf448e4886b1b3bfbefac70b
Gilad Ehven <gilad@ehven.com>
2019-10-06 16:49:56 -0400

    Completed base implementation of 'Footer' template.

--- footer.php
+++ footer.php
@@ -4,9 +4,43 @@
 
     if ( ! defined( 'ABSPATH' ) ) exit( 'Nothing to see here. Go <a href="/">home</a>.' );
 
-    wp_footer(); ?>
+    require_once( TYPE8_CORE_PARTIAL . '/footer.php' );
+
+    class Template_Footer extends CORE_Footer {
+
+        public function __construct() {
+
+            parent::__construct();
+
+        }
+
+    }
+
+    $template_footer = new Template_Footer();
+
+    $template_footer->ah_main_end(); ?>
 
 
+
+            </div>
+
+        </main>
+
+        <?php $template_footer->ah_main_after(); ?>
+
+        <footer class="<?php $template_footer->fh_body_footer_classes(); ?>" id="body-footer">
+
+            <?php $template_footer->ah_body_footer(); ?>
+
+        </footer>
+
+        <?php
+
+            $template_footer->ah_body_end();
+
+            wp_footer();
+
+        ?>
 
     </body>
 
