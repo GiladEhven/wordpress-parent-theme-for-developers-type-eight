81243202c0b230af57c5ac02a1c9bf83d6131b7e
Gilad Ehven <gilad@ehven.com>
2019-10-10 22:17:42 -0400

    Implemented 'Bootstrap' support (v 4.3.1 + 'Popper' 1.14.7).

--- CORE/core/website/template.php
+++ CORE/core/website/template.php
@@ -12,9 +12,54 @@
 
             parent::__construct();
 
+            $this->enqueue_bootstrap();
+
             $this->set_browser_classes();
 
             $this->set_template_properties();
+
+        }
+
+        protected function enqueue_bootstrap() {
+
+            add_action( 'wp_enqueue_scripts', function() {
+
+                wp_enqueue_script( 'bootstrap-script', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js',    array( 'jquery', 'popper-script' ), null, true );
+                wp_enqueue_script( 'popper-script',    'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js', array( 'jquery' ),                  null, true );
+
+                wp_enqueue_style(  'bootstrap-style',  'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',  array(),                            null, 'all' );
+
+            });
+
+            add_filter( 'script_loader_tag', function ( $tag, $handle, $src ) {
+
+                $integrity = '';
+
+                if ( $handle == 'bootstrap-script' ) {
+
+                    $tag = '<script id="' . $handle . '" src="' . $src . '" type="text/javascript" crossorigin="anonymous" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"></script>' . "\n";
+
+                } elseif ( $handle == 'popper-script' ) {
+
+                    $tag = '<script id="' . $handle . '" src="' . $src . '" type="text/javascript" crossorigin="anonymous" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"></script>' . "\n";
+
+                }
+
+                return $tag;
+
+            }, 10, 3);
+
+            add_filter( 'style_loader_tag', function( $link, $handle ) {
+
+                if ( $handle == 'bootstrap-style' ) {
+
+                    $link = str_replace( 'media=\'all\' />', 'media=\'all\' crossorigin="anonymous" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" />', $link );
+
+                }
+
+                return $link;
+
+            }, 10, 2);
 
         }
 
