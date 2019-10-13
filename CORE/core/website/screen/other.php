f1a300ced7ce7910b48113935894924facf061d2
Gilad Ehven <gilad@ehven.com>
2019-10-13 03:05:40 -0400

    Added parent/child handler for dashboard widget: 'Prepare Graphics' instructions.

--- CORE/core/website/screen/other.php
+++ CORE/core/website/screen/other.php
@@ -12,7 +12,28 @@
 
             parent::__construct();
 
+            $this->add_dashboard_widgets();
+
             $this->set_other_properties();
+
+        }
+
+        protected function add_dashboard_widgets() {
+
+            add_action( 'wp_dashboard_setup', function() {
+
+                wp_add_dashboard_widget( 'prepare_graphics_widget', 'Prepare Graphics', function() {
+
+                    $dash_widget_child  = get_stylesheet_directory() . '/statics/php/prepare-graphics.php';
+                    $dash_widget_parent = get_template_directory()   . '/CORE/defaults/php/prepare-graphics.php';
+
+                    $admin_widget_to_require = ( file_exists( $dash_widget_child ) ) ? $dash_widget_child : $dash_widget_parent;
+
+                    require_once( $admin_widget_to_require );
+
+                });
+
+            });
 
         }
 
---
+++ CORE/defaults/php/prepare-graphics.php
@@ -0,0 +1,2 @@
+<p>This placeholder should be replaced with instructions for preparing graphics for use on this site.</p>
+<p>Place instructions in your child theme at <code>/statics/php/prepare-graphics.php</code> to automatically replace this placeholder text.</p>
