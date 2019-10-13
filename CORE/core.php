00e69d6553d96a8aea839a1417189b862e52ed08
Gilad Ehven <gilad@ehven.com>
2019-10-13 03:37:40 -0400

    Cleaned up after moving 'ah_comments_*' theme action hook methods.

--- CORE/core.php
+++ CORE/core.php
@@ -88,23 +88,7 @@
         public function ah_video_entry_content_before()      { do_action( 'ah_video_entry_content_before' );      }
         public function ah_video_entry_footer()              { do_action( 'ah_video_entry_footer' );              }
         public function ah_video_entry_header()              { do_action( 'ah_video_entry_header' );              }
-/*
-        public function ah_comments_after()                  { do_action( 'ah_comments_after' );                  }
-        public function ah_comments_before()                 { do_action( 'ah_comments_before' );                 }
-        public function ah_comments_begin()                  { do_action( 'ah_comments_begin' );                  }
-        public function ah_comments_else_begin()             { do_action( 'ah_comments_else_begin' );             }
-        public function ah_comments_else_end()               { do_action( 'ah_comments_else_end' );               }
-        public function ah_comments_else_header()            { do_action( 'ah_comments_else_header' );            }
-        public function ah_comments_else_section()           { do_action( 'ah_comments_else_section' );           }
-        public function ah_comments_footer_begin()           { do_action( 'ah_comments_footer_begin' );           }
-        public function ah_comments_footer_end()             { do_action( 'ah_comments_footer_end' );             }
-        public function ah_comments_header_begin()           { do_action( 'ah_comments_header_begin' );           }
-        public function ah_comments_header_end()             { do_action( 'ah_comments_header_end' );             }
-        public function ah_comments_if_begin()               { do_action( 'ah_comments_if_begin' );               }
-        public function ah_comments_if_end()                 { do_action( 'ah_comments_if_end' );                 }
-        public function ah_comments_list_begin()             { do_action( 'ah_comments_list_begin' );             }
-        public function ah_comments_list_end()               { do_action( 'ah_comments_list_end' );               }
-*/
+
         public function fh_body_footer_classes()             { echo apply_filters( 'fh_body_footer_classes',             'container-fluid' );    }
         public function fh_body_header_classes()             { echo apply_filters( 'fh_body_header_classes',             'container-fluid' );    }
         public function fh_body_main_classes()               { echo apply_filters( 'fh_body_main_classes',               'container' );          }
