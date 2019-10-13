1fd6444d7f3bf4d639c33319768ac25a1121eb44
Gilad Ehven <gilad@ehven.com>
2019-10-13 00:27:35 -0400

    Added simple handler for parent/child admin styles.

--- CORE/core/website/screen.php
+++ CORE/core/website/screen.php
@@ -12,7 +12,25 @@
 
             parent::__construct();
 
+            $this->enqueue_for_admin();
+
             $this->set_screen_properties();
+
+        }
+
+        protected function enqueue_for_admin() {
+
+            $admin_style_child_path  = get_stylesheet_directory()     . '/statics/css/admin.css';
+            $admin_style_child_url   = get_stylesheet_directory_uri() . '/statics/css/admin.css';
+            $admin_style_parent_url  = get_template_directory_uri()   . '/CORE/defaults/css/admin.css';
+
+            $admin_style_to_enqueue = ( file_exists( $admin_style_child_path ) ) ? $admin_style_child_url : $admin_style_parent_url;
+
+            add_action( 'admin_enqueue_scripts', function() use ( $admin_style_to_enqueue ) {
+
+                wp_enqueue_style(  'custom-admin-style', $admin_style_to_enqueue, array(), null, 'all' );
+
+            });
 
         }
 
