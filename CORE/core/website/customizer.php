ac1db3ee2703808601939c9981515e0687539163
Gilad Ehven <gilad@ehven.com>
2019-10-29 23:24:03 -0400

    Added settings and controls (only) with new 'Customizer' section: 'Sitewide Social Profiles'.

--- CORE/core/website/customizer.php
+++ CORE/core/website/customizer.php
@@ -34,6 +34,8 @@
 
             add_action( 'customize_register', function( $wp_customize ) {
 
+                // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
                 $wp_customize->add_setting( 'background_color' , array(
                     'capability'           => 'edit_theme_options',
                     'default'              => '#ffffff',
@@ -55,6 +57,8 @@
                     'settings'             => 'background_color',
                 )));
 
+                // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
                 $wp_customize->add_section( 'type_eight_colors' , array(
                     'active_callback'      => '',
                     'capability'           => 'edit_theme_options',
@@ -65,6 +69,224 @@
                     'theme_supports'       => '',
                     'title'                => __( 'Type Eight (Parent Theme) Colors' ),
                 ));
+
+                // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = //
+
+                $wp_customize->add_setting( 'type_eight_sitewide_social_facebook' , array(
+                    'capability'           => 'edit_theme_options',
+                    'default'              => '',
+                    'dirty'                => false,
+                    'sanitize_callback'    => '',
+                    'sanitize_js_callback' => '',
+                    'theme_supports'       => '',
+                    'transport'            => 'refresh',
+                    'type'                 => 'theme_mod',
+                    'validate_callback'    => '',
+                ));
+
+                $wp_customize->add_control( 'type_eight_sitewide_social_facebook', array(
+                    'capability'           => 'edit_theme_options',
+                    'description'          => esc_html__( 'Facebook page for this site' ),
+                    'input_attrs'          => array(
+                        'class'            => 'type-eight-customizer type-eight-text type-eight-facebook',
+                        'style'            => '',
+                        'placeholder'      => __( 'Facebook Page...' ),
+                        ),
+                    'label'                => __( 'Sitewide Facebook Profile' ),
+                    'priority'             => 10,
+                    'section'              => 'type_eight_sitewide_social_profiles',
+                    'settings'             => 'type_eight_sitewide_social_facebook',
+                    'type'                 => 'text',
+                ));
+
+                // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+                $wp_customize->add_setting( 'type_eight_sitewide_social_github' , array(
+                    'capability'           => 'edit_theme_options',
+                    'default'              => '',
+                    'dirty'                => false,
+                    'sanitize_callback'    => '',
+                    'sanitize_js_callback' => '',
+                    'theme_supports'       => '',
+                    'transport'            => 'refresh',
+                    'type'                 => 'theme_mod',
+                    'validate_callback'    => '',
+                ));
+
+                $wp_customize->add_control( 'type_eight_sitewide_social_github', array(
+                    'capability'           => 'edit_theme_options',
+                    'description'          => esc_html__( 'Github account for this site' ),
+                    'input_attrs'          => array(
+                        'class'            => 'type-eight-customizer type-eight-text type-eight-github',
+                        'style'            => '',
+                        'placeholder'      => __( 'GitHub Address...' ),
+                        ),
+                    'label'                => __( 'Sitewide GitHub Account' ),
+                    'priority'             => 10,
+                    'section'              => 'type_eight_sitewide_social_profiles',
+                    'settings'             => 'type_eight_sitewide_social_github',
+                    'type'                 => 'text',
+                ));
+
+                // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+                $wp_customize->add_setting( 'type_eight_sitewide_social_instagram' , array(
+                    'capability'           => 'edit_theme_options',
+                    'default'              => '',
+                    'dirty'                => false,
+                    'sanitize_callback'    => '',
+                    'sanitize_js_callback' => '',
+                    'theme_supports'       => '',
+                    'transport'            => 'refresh',
+                    'type'                 => 'theme_mod',
+                    'validate_callback'    => '',
+                ));
+
+                $wp_customize->add_control( 'type_eight_sitewide_social_instagram', array(
+                    'capability'           => 'edit_theme_options',
+                    'description'          => esc_html__( 'Instagram account for this site' ),
+                    'input_attrs'          => array(
+                        'class'            => 'type-eight-customizer type-eight-text type-eight-instagram',
+                        'style'            => '',
+                        'placeholder'      => __( 'Instagram Profile...' ),
+                        ),
+                    'label'                => __( 'Sitewide instagram Account' ),
+                    'priority'             => 10,
+                    'section'              => 'type_eight_sitewide_social_profiles',
+                    'settings'             => 'type_eight_sitewide_social_instagram',
+                    'type'                 => 'text',
+                ));
+
+                // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+                $wp_customize->add_setting( 'type_eight_sitewide_social_linkedin' , array(
+                    'capability'           => 'edit_theme_options',
+                    'default'              => '',
+                    'dirty'                => false,
+                    'sanitize_callback'    => '',
+                    'sanitize_js_callback' => '',
+                    'theme_supports'       => '',
+                    'transport'            => 'refresh',
+                    'type'                 => 'theme_mod',
+                    'validate_callback'    => '',
+                ));
+
+                $wp_customize->add_control( 'type_eight_sitewide_social_linkedin', array(
+                    'capability'           => 'edit_theme_options',
+                    'description'          => esc_html__( 'LinkedIn page for this site' ),
+                    'input_attrs'          => array(
+                        'class'            => 'type-eight-customizer type-eight-text type-eight-linkedin',
+                        'style'            => '',
+                        'placeholder'      => __( 'LinkedIn Business Page...' ),
+                        ),
+                    'label'                => __( 'Sitewide LinkedIn Business Page' ),
+                    'priority'             => 10,
+                    'section'              => 'type_eight_sitewide_social_profiles',
+                    'settings'             => 'type_eight_sitewide_social_linkedin',
+                    'type'                 => 'text',
+                ));
+
+                // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+                $wp_customize->add_setting( 'type_eight_sitewide_social_pinterest' , array(
+                    'capability'           => 'edit_theme_options',
+                    'default'              => '',
+                    'dirty'                => false,
+                    'sanitize_callback'    => '',
+                    'sanitize_js_callback' => '',
+                    'theme_supports'       => '',
+                    'transport'            => 'refresh',
+                    'type'                 => 'theme_mod',
+                    'validate_callback'    => '',
+                ));
+
+                $wp_customize->add_control( 'type_eight_sitewide_social_pinterest', array(
+                    'capability'           => 'edit_theme_options',
+                    'description'          => esc_html__( 'Pinterest account for this site' ),
+                    'input_attrs'          => array(
+                        'class'            => 'type-eight-customizer type-eight-text type-eight-pinterest',
+                        'style'            => '',
+                        'placeholder'      => __( 'Pinterest Profile...' ),
+                        ),
+                    'label'                => __( 'Sitewide Pinterest Account' ),
+                    'priority'             => 10,
+                    'section'              => 'type_eight_sitewide_social_profiles',
+                    'settings'             => 'type_eight_sitewide_social_pinterest',
+                    'type'                 => 'text',
+                ));
+
+                // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+                $wp_customize->add_setting( 'type_eight_sitewide_social_twitter' , array(
+                    'capability'           => 'edit_theme_options',
+                    'default'              => '',
+                    'dirty'                => false,
+                    'sanitize_callback'    => '',
+                    'sanitize_js_callback' => '',
+                    'theme_supports'       => '',
+                    'transport'            => 'refresh',
+                    'type'                 => 'theme_mod',
+                    'validate_callback'    => '',
+                ));
+
+                $wp_customize->add_control( 'type_eight_sitewide_social_twitter', array(
+                    'capability'           => 'edit_theme_options',
+                    'description'          => esc_html__( 'Twitter page for this site' ),
+                    'input_attrs'          => array(
+                        'class'            => 'type-eight-customizer type-eight-text type-eight-twitter',
+                        'style'            => '',
+                        'placeholder'      => __( 'Twitter Page...' ),
+                        ),
+                    'label'                => __( 'Sitewide Twitter Profile' ),
+                    'priority'             => 10,
+                    'section'              => 'type_eight_sitewide_social_profiles',
+                    'settings'             => 'type_eight_sitewide_social_twitter',
+                    'type'                 => 'text',
+                ));
+
+                // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+                $wp_customize->add_setting( 'type_eight_sitewide_social_youtube' , array(
+                    'capability'           => 'edit_theme_options',
+                    'default'              => '',
+                    'dirty'                => false,
+                    'sanitize_callback'    => '',
+                    'sanitize_js_callback' => '',
+                    'theme_supports'       => '',
+                    'transport'            => 'refresh',
+                    'type'                 => 'theme_mod',
+                    'validate_callback'    => '',
+                ));
+
+                $wp_customize->add_control( 'type_eight_sitewide_social_youtube', array(
+                    'capability'           => 'edit_theme_options',
+                    'description'          => esc_html__( 'YouTube channel for this site' ),
+                    'input_attrs'          => array(
+                        'class'            => 'type-eight-customizer type-eight-text type-eight-youtube',
+                        'style'            => '',
+                        'placeholder'      => __( 'YouTube Channel...' ),
+                        ),
+                    'label'                => __( 'Sitewide YouTube Channel' ),
+                    'priority'             => 10,
+                    'section'              => 'type_eight_sitewide_social_profiles',
+                    'settings'             => 'type_eight_sitewide_social_youtube',
+                    'type'                 => 'text',
+                ));
+
+                // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+                $wp_customize->add_section( 'type_eight_sitewide_social_profiles' , array(
+                    'active_callback'      => '',
+                    'capability'           => 'edit_theme_options',
+                    'description'          => esc_html__( 'Type Eight Parent Sitewide Social Profiles' ),
+                    'description_hidden'   => 'false',
+                    'panel'                => 'type_eight_customizations',
+                    'priority'             => 120,
+                    'theme_supports'       => '',
+                    'title'                => __( 'Type Eight Sitewide Social Profiles' ),
+                ));
+
+                // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = //
 
                 $wp_customize->add_panel( 'type_eight_customizations' , array(
                     'active_callback'      => '',
